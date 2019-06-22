d7be76e8ed5b57bcb25a561f5acaa605de6a4a3b
Chris Oakman <chris@oakmac.com>
2019-06-22 08:47:25 -0500

    saving state on objects

---
+++ exercises/110-objects.js
@@ -0,0 +1,27 @@
+//
+// Objects are key => value pairs of things. Similar to a Python Dictionary or Ruby Hash.
+//
+
+
+
+// Objects are fundamental in JavaScript: everything in JavaScript is an object
+// unless it is a simple type (Number, String, Boolean, null, undefined).
+//
+// Yes - that means that technically Arrays and Functions are Objects!
+//
+// Objects can be complex, but for these exercises we will focus on the basics.
+//
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Basics
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// You can create an Object using the {} characters.
+// Return "myObj" below
+
+function basicObject () {
+  const myObj = {a: 'Alpha', b: 'Bravo', c: 'Charlie'}
+
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// You can create an Object using the {} characters.
+// Return "myObj" below
